ce15dd7c21fbcf4c7c18820f176821e394d918a6
brahdian <vishal.vasistha1@gmail.com>
2025-02-03 08:52:02 -0400

    Add NeuroFluxTrainer to package exports
    - Include GRPOTrainer as NeuroFluxTrainer in training module exports
    - Update __all__ list to expose the new trainer class
    - Maintain consistent package initialization pattern

--- neuroflux/training/__init__.py
+++ neuroflux/training/__init__.py
@@ -1,4 +1,5 @@
 from .curriculum import EnhancedCurriculumManager
 from .evaluators import CodeGenerationEvaluator
+from .trainers import GRPOTrainer as NeuroFluxTrainer
 
-__all__ = ['EnhancedCurriculumManager', 'CodeGenerationEvaluator'] 
+__all__ = ['EnhancedCurriculumManager', 'CodeGenerationEvaluator', 'NeuroFluxTrainer'] 
